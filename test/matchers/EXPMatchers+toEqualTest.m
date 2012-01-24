14fb9f6123ad6b6050c9e7de5b4c9ac5f1b234f1
Josh Vera <josh@joshvera.com>
2012-01-24 14:55:40 -0800

    Expand test_toEqual_CGRect test in toEqual matcher.

--- test/matchers/EXPMatchers+toEqualTest.m
+++ test/matchers/EXPMatchers+toEqualTest.m
@@ -241,10 +241,22 @@ typedef struct SomeFloatPairPair {
     assertPass(test_expect(a).toEqual(b));
 }
 
-- (void)test_toEqual_CGRect {
+- (void)test_toEqual_CGRectNull {
     CGRect a = CGRectNull;
     CGRect b = CGRectNull;
     assertPass(test_expect(a).toEqual(b));
+}
+
+- (void)test_toEqual_CGRect {
+    CGRect a = CGRectMake(0.0f, 0.0f, 50.0f, 50.0f);
+    CGRect b = CGRectMake(0.0f, 0.0f, 50.0f, 50.0f);
+    assertPass(test_expect(a).toEqual(b));
+
+    CGRect failRect = CGRectMake(0.0f, 0.0f, 50.0f, 50.1f);
+    assertFail(test_expect(a).toEqual(failRect));
+
+    assertFail(test_expect(a).toEqual(CGRectInfinite));
+    assertFail(test_expect(a).toEqual(CGRectNull));
 }
 
 @end
