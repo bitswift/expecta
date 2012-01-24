02067516f025b882ed6f96317469ebb442eb3e09
Josh Vera <josh@joshvera.com>
2012-01-24 14:21:59 -0800

    Conditionalize EXPIsValueRect based on OS platform.

--- src/matchers/EXPMatcherHelpers.m
+++ src/matchers/EXPMatcherHelpers.m
@@ -9,5 +9,9 @@ BOOL EXPIsNumberFloat(NSNumber *number) {
 }
 
 BOOL EXPIsValueRect(NSValue *value) {
+#ifdef __MAC_OS_X_VERSION_MAX_ALLOWED
     return strcmp([value objCType], @encode(CGRect)) == 0 || strcmp([value objCType], @encode(NSRect)) == 0;
+#else
+    return strcmp([value objCType], @encode(CGRect)) == 0;
+#endif
 }
