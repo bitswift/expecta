8400afda086f044517728abc51f81b4a498fc5eb
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-02-04 14:12:11 -0800

    Reformatted/fixed up toSupportArchiving matcher to match the conventions of the rest of the project

--- src/matchers/EXPMatchers+toSupportArchiving.m
+++ src/matchers/EXPMatchers+toSupportArchiving.m
@@ -1,44 +1,36 @@
-//
-//  EXPMatchers+toSupportArchiving.m
-//  Expecta
-//
-//  Created by Josh Vera on 1/30/12.
-//  Copyright (c) 2012 Peter Jihoon Kim. All rights reserved.
-//
-
 #import "EXPMatchers+toSupportArchiving.h"
 
 EXPMatcherImplementationBegin(toSupportArchiving, (void)) {
-    BOOL actualIsNil = (actual == nil);
-    
-    prerequisite(^BOOL{
-        return !(actualIsNil);
-    });
-    
-    match(^BOOL{
-        if (![actual conformsToProtocol:@protocol(NSCoding)])
-            return NO;
-        
-        NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:actual];
-        if (!archivedData)
-            return NO;
-        
-        id expected = [NSKeyedUnarchiver unarchiveObjectWithData:archivedData];
-        if (!expected)
-            return NO;
-        
-        return [actual isEqual:expected];
-    });
-    
-    failureMessageForTo(^NSString *{
-        if(actualIsNil) return @"the actual value is nil/null";
-        return @"the actual value does not conform to <NSCoding>";
-    });
-    
-    failureMessageForNotTo(^NSString *{
-        if(actualIsNil) return @"the actual value is nil/null";
-        return @"the actual value does not conform to <NSCoding>";        
-    });
+  BOOL actualIsNil = (actual == nil);
+
+  prerequisite(^BOOL{
+    return !(actualIsNil);
+  });
+
+  match(^BOOL{
+    if (![actual conformsToProtocol:@protocol(NSCoding)])
+      return NO;
+
+    NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:actual];
+    if (!archivedData)
+      return NO;
+
+    id expected = [NSKeyedUnarchiver unarchiveObjectWithData:archivedData];
+    if (!expected)
+      return NO;
+
+    return [actual isEqual:expected];
+  });
+
+  failureMessageForTo(^NSString *{
+    if(actualIsNil) return @"the actual value is nil/null";
+    return [NSString stringWithFormat:@"expected: an object implementing <NSCoding>, got: an instance of %@, which does not support archiving", [actual class]];
+  });
+
+  failureMessageForNotTo(^NSString *{
+    if(actualIsNil) return @"the actual value is nil/null";
+    return [NSString stringWithFormat:@"expected: an object not implementing <NSCoding>, got: an instance of %@, which does support archiving", [actual class]];
+  });
 }
 
 EXPMatcherImplementationEnd
--- test/matchers/EXPMatchers+toSupportArchivingTest.m
+++ test/matchers/EXPMatchers+toSupportArchivingTest.m
@@ -1,17 +1,8 @@
-//
-//  EXPMatchers+toSupportArchiving.m
-//  Expecta
-//
-//  Created by Josh Vera on 1/30/12.
-//  Copyright (c) 2012 Peter Jihoon Kim. All rights reserved.
-//
-
 #import "TestHelper.h"
 
-
 @interface EXPMatchers_toSupportArchivingTest : SenTestCase {
-    Foo *foo;
-    EncodableObject *encodableObject;
+  Foo *foo;
+  EncodableObject *encodableObject;
 }
 
 @end
@@ -19,16 +10,16 @@
 @implementation EXPMatchers_toSupportArchivingTest
 
 - (void)setUp {
-    foo = [[Foo new] autorelease];
-    encodableObject = [[[EncodableObject alloc] initWithString:@"EncodableString" array:[NSArray arrayWithObject:@"Hello"]] autorelease];
+  foo = [[Foo new] autorelease];
+  encodableObject = [[[EncodableObject alloc] initWithString:@"EncodableString" array:[NSArray arrayWithObject:@"Hello"]] autorelease];
 }
 
 - (void)test_toSupportArchiving {
-    assertPass(test_expect(encodableObject).toSupportArchiving());
+  assertPass(test_expect(encodableObject).toSupportArchiving());
 }
 
 - (void)test_Not_toSupportArchiving {
-    assertPass(test_expect(foo).Not.toSupportArchiving());
+  assertPass(test_expect(foo).Not.toSupportArchiving());
 }
 
 @end
