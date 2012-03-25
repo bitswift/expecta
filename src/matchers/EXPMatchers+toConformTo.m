92233fad3480f6ca68aef0019fd58d5adb86afd8
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-03-25 14:28:06 -0700

    Check that an object responds to all the methods in a protocol when using toConformTo()

--- src/matchers/EXPMatchers+toConformTo.m
+++ src/matchers/EXPMatchers+toConformTo.m
@@ -5,12 +5,47 @@ EXPMatcherImplementationBegin(toConformTo, (Protocol *expected)) {
   BOOL actualIsNil = (actual == nil);
   BOOL expectedIsNil = (expected == nil);
 
+  __block SEL missingMethod = NULL;
+  __block BOOL missingMethodIsInstanceMethod = NO;
+
   prerequisite(^BOOL{
     return !(actualIsNil || expectedIsNil);
   });
 
   match(^BOOL{
-    return [actual conformsToProtocol:expected];
+    if (![actual conformsToProtocol:expected])
+      return NO;
+
+    // make sure the object responds to all the required instance methods
+    unsigned instanceMethodCount = 0;
+    struct objc_method_description *instanceMethodDescriptions = protocol_copyMethodDescriptionList(expected, YES, YES, &instanceMethodCount);
+    
+    for (unsigned i = 0; i < instanceMethodCount; ++i) {
+      SEL name = instanceMethodDescriptions[i].name;
+      if (![actual respondsToSelector:name]) {
+        missingMethod = name;
+        missingMethodIsInstanceMethod = YES;
+        return NO;
+      }
+    }
+
+    free(instanceMethodDescriptions);
+
+    // make sure the class responds to all the required class methods
+    unsigned classMethodCount = 0;
+    struct objc_method_description *classMethodDescriptions = protocol_copyMethodDescriptionList(expected, YES, NO, &classMethodCount);
+    
+    for (unsigned i = 0; i < classMethodCount; ++i) {
+      SEL name = classMethodDescriptions[i].name;
+      if (![actual respondsToSelector:name])
+        missingMethod = name;
+        missingMethodIsInstanceMethod = NO;
+        return NO;
+    }
+
+    free(classMethodDescriptions);
+
+    return YES;
   });
 
   failureMessageForTo(^NSString *{
@@ -18,7 +53,19 @@ EXPMatcherImplementationBegin(toConformTo, (Protocol *expected)) {
     if(expectedIsNil) return @"the expected value is nil/null";
 
     const char *name = protocol_getName(expected);
-    return [NSString stringWithFormat:@"expected: an object conforming to <%s>, got: an instance of %@, which does not conform to <%s>", name, [actual class], name];
+
+    if (missingMethod) {
+      NSString *missingMethodName = NSStringFromSelector(missingMethod);
+
+      if (missingMethodIsInstanceMethod)
+        missingMethodName = [@"-" stringByAppendingString:missingMethodName];
+      else
+        missingMethodName = [@"+" stringByAppendingString:missingMethodName];
+
+      return [NSString stringWithFormat:@"expected: an object conforming to <%s>, got: an instance of %@, which does not respond to %@", name, [actual class], missingMethodName];
+    } else {
+      return [NSString stringWithFormat:@"expected: an object conforming to <%s>, got: an instance of %@, which does not conform to <%s>", name, [actual class], name];
+    }
   });
 
   failureMessageForNotTo(^NSString *{
